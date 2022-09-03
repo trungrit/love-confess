50b2c6cc51329fe4f875c7e2123dee65f36cc429
trungrit <112764777+trungrit@users.noreply.github.com>
2022-09-03 20:40:26 +0700

    love
    confess

---
+++ still-single-main/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Love confess",
+    introTitle: 'Đia Mai Eng Dồ ^^',
+    introDesc: "Meeting you was one of the best time in my life ^^ I mean you showed me something I've never felt before🥺 You truly made me fall in love with you without even trying",
+    btnIntro: '^^HiHi^^',
+    title: 'Đố biết ai đang nhớ em vl nào? 🥰',
+    desc: '',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Cho anh biết lí do đi',
+    btnReply: 'Gửi cho Anh iu <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu',
+    mess: 'Anh biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
+    messDesc: 'Nhiều vailonn ^^.',
+    btnAccept: 'Em cũng yêu anh nhiều vailonn <3',
+    messLink: 'https://www.facebook.com/trung.caothe.5'
+}
